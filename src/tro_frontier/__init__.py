245d4b558f68a2d000dd5e5a9bfe725f9502c09e
chugarchugarr <josephlerma19@gmail.com>
2026-07-24 23:57:07 -0500

    Expose v0.2 routing and ablation APIs

--- src/tro_frontier/__init__.py
+++ src/tro_frontier/__init__.py
@@ -1,8 +1,20 @@
-"""TRACE + REVAL + ORO frontier-code adapter."""
+"""TRACE + REVAL + ORO benchmark adapters."""
 
+from .ablation import VARIANTS, run_component_ablation
 from .adapter import run_task
 from .config import FrozenManifest, load_manifest
+from .routing import ModelRouter, ModelTier
 from .runner import AgentRunner, TaskSpec
 
-__all__ = ["AgentRunner", "FrozenManifest", "TaskSpec", "load_manifest", "run_task"]
-__version__ = "0.1.0"
+__all__ = [
+    "AgentRunner",
+    "FrozenManifest",
+    "ModelRouter",
+    "ModelTier",
+    "TaskSpec",
+    "VARIANTS",
+    "load_manifest",
+    "run_component_ablation",
+    "run_task",
+]
+__version__ = "0.2.0"
