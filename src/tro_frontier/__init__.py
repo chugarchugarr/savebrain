81d5fbbb40daa5ae6fa4a48e2a51885ffef34378
chugarchugarr <josephlerma19@gmail.com>
2026-09-03 22:07:07 -0500

    Retire savebrain implementation

--- src/tro_frontier/__init__.py
+++
@@ -1,8 +0,0 @@
-"""TRACE + REVAL + ORO frontier-code adapter."""
-
-from .adapter import run_task
-from .config import FrozenManifest, load_manifest
-from .runner import AgentRunner, TaskSpec
-
-__all__ = ["AgentRunner", "FrozenManifest", "TaskSpec", "load_manifest", "run_task"]
-__version__ = "0.1.0"
